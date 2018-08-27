4a307f7609c306048ac6a683d17117a2e35a8301
Nick Cerdan <ncerdan1229@gmail.com>
2018-08-26 21:12:33 -0700

    test format

--- app/src/main/java/com/nickcerdan/myswipes/MainActivity.java
+++ app/src/main/java/com/nickcerdan/myswipes/MainActivity.java
@@ -2,12 +2,33 @@ package com.nickcerdan.myswipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    int swipesLeft;
+    Button swipeBtn;
+    EditText swipesLeftNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeBtn = (Button) findViewById(R.id.swipeBtn);
+        swipesLeftNum = (EditText) findViewById(R.id.swipesLeftNum);
+        swipesLeft = 70;
+
+        swipesLeftNum.setText(String.valueOf(swipesLeft));
+
+        swipeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swipesLeft = swipesLeft - 1;
+                swipesLeftNum.setText(String.valueOf(swipesLeft));
+            }
+        });
     }
 }
